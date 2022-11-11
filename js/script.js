fd3c3c8836b954b763686cad107ba85aef338db6
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2022-11-11 12:08:35 -0700

    Add operate() function.
    Added functionality for future wiring up to the HTML calculator.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (true) {
+    case (operator === '+'):
+      return add(a, b);
+      break;
+    case (operator === '-'):
+      return subtract(a, b);
+      break;
+    case (operator === '*'):
+      return multiply(a, b);
+      break;
+    case (operator === '/'):
+      return divide(a, b);
+      break;
+  }
+}
